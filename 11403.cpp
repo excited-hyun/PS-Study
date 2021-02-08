017d307f5d665d5e1a8f638068af7f6b69518406
excited-hyun <nahyun1127@naver.com>
2021-02-08 23:34:01 +0900

    BFS

--- 11403.cpp
+++ 11403.cpp
@@ -3,13 +3,51 @@
 #include <queue>
 
 using namespace std;
-int step[300];
+int way[100][100];
+int can[100][100];
+
+queue<int> q;
+
+void bfs(int s, int n);
 
 int main(void){
     int n;
     scanf("%d", &n);
     for(int i=0; i<n; i++){
-        scanf("%d", &step[i]);
+        for(int j=0; j<n; j++)
+            scanf("%d", &way[i][j]);
+    }
+    for(int i=0; i<n; i++){
+        bfs(i, n);
     }
     
+    for(int i=0; i<n; i++){
+        for(int j=0; j<n; j++)
+            printf("%d ", can[i][j]);
+        
+        printf("\n");
+    }
+}
+
+
+void bfs(int s, int n){
+    int visited[100];
+    int here;
+    for(int i=0; i<n; i++){
+        visited[i] = 0;
+    }
+    q.push(s);
+    
+    while(!q.empty()){
+        here = q.front();
+        q.pop();
+        for(int i=0; i<n; i++){
+            if(way[here][i] == 0 || visited[i] == 1)
+                continue;
+            q.push(i);
+            
+            can[s][i] = 1;
+            visited[i] = 1;
+        }
+    }
 }
