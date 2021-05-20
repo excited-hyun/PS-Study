974e5e2926aea969a7cc171b591c9e568970e382
excited-hyun <nahyun1127@naver.com>
2021-05-21 01:04:50 +0900

    binary search

--- 2776.cpp
+++ 2776.cpp
@@ -1,9 +1,54 @@
-//
-//  2776.cpp
-//  BeakJoon
-//
-//  Created by 신나현 on 2021/05/20.
-//  Copyright © 2021 신나현. All rights reserved.
-//
+#include <iostream>
+#include <cstdio>
+#include <algorithm>
+#include <vector>
 
-#include <stdio.h>
+using namespace std;
+
+vector<int> memo_1;
+int n;
+int m;
+
+void sol(int find){
+    int low = 0;
+    int high = n-1;
+    int mid;
+    
+    while(high >= low){
+        mid = (low + high) / 2;
+        
+        if(find > memo_1[mid])
+            low = mid+1;
+        else if(find < memo_1[mid])
+            high = mid-1;
+        else{
+            printf("1\n");
+            return ;
+        }
+    }
+    printf("0\n");
+}
+
+int main(void){
+    int tc;
+    int num;
+    
+    scanf("%d", &tc);
+    
+    for(int i=0; i<tc; i++){
+        scanf("%d", &n);
+        for(int j=0; j<n; j++){
+            scanf("%d", &num);
+            memo_1.push_back(num);
+        }
+        sort(memo_1.begin(), memo_1.end());
+        
+        scanf("%d", &m);
+        for(int j=0; j<m; j++){
+            scanf("%d", &num);
+            sol(num);
+            
+        }
+        memo_1.clear();
+    }
+}
